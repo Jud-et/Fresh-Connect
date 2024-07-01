ceeb50870e86f9b880fe9bf35bb3a1e85d7c9c96
h3nryK <henrykimani89@gmail.com>
2024-07-01 21:08:19 +0300

    routes

--- src/fresh_connect_frontend/src/agent.jsx
+++ src/fresh_connect_frontend/src/agent.jsx
@@ -1,6 +1,6 @@
 import { HttpAgent, Actor } from '@dfinity/agent';
-import { idlFactory } from '../../declarations/fresh_farm_connect_backend';
-import { canisterId as backendCanisterId } from '../../../.dfx/local/canisters/fresh_connect_backend';
+import { idlFactory } from '../../declarations/fresh_connect_backend';
+import { canisterId as backendCanisterId } from '../../declarations/fresh_connect_backend';
 
 const agent = new HttpAgent({ host: 'http://localhost:8000' });
 
